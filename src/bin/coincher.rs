35b2f3b0074c6ab7dc31c421402eeb5273dca6e5
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2021-10-20 21:30:02 +0000

    Fix route

--- src/bin/coincher.rs
+++ src/bin/coincher.rs
@@ -64,3 +64,68 @@ impl client::Frontend<client::http::HttpBackend> for CliFrontend {
     fn show_error(&mut self, error: client::http::Error) {
         println!("Error: {:?}", error);
     }
+
+    fn unexpected_event(&mut self, event: EventType) {
+        println!("Unexpected event: {:?}", event);
+    }
+
+    fn party_cancelled(&mut self, msg: &str) {
+        println!("Party cancelled: {}", msg);
+    }
+
+    fn show_card_played(&mut self, pos: pos::PlayerPos, card: cards::Card) {
+        println!("Player {:?} played {}", pos, card.to_string());
+        if pos == self.pos {
+            self.hand.remove(card);
+        }
+    }
+
+    fn show_trick_over(&mut self, winner: pos::PlayerPos) {
+        println!("{:?} gets the trick.", winner);
+    }
+
+    fn ask_card(&mut self) -> client::GameAction {
+        let cards = self.hand.list();
+
+        loop {
+            self.print_hand();
+            print!("What card do you play?\n> ");
+            io::stdout().flush().unwrap();
+
+            let line = Self::input();
+
+            if line == "leave" {
+                return client::GameAction::Leave;
+            } else {
+                match usize::from_str(&line) {
+                    Ok(i) if i < cards.len() => return client::GameAction::PlayCard(cards[i]),
+                    _ => println!("Invalid input."),
+                }
+            }
+        }
+    }
+
+    fn game_over(&mut self, points: [i32; 2], winner: pos::Team, scores: [i32; 2]) {
+        println!("Game over!");
+        println!("{:?} won. Points were {:?} ; scores: {:?}",
+                 winner,
+                 points,
+                 scores);
+    }
+
+    fn show_pass(&mut self, pos: pos::PlayerPos) {
+        println!("Player {:?} passed", pos);
+    }
+
+    fn show_coinche(&mut self, pos: pos::PlayerPos) {
+        println!("Player {:?} coinched", pos);
+    }
+
+    fn show_bid(&mut self, pos: pos::PlayerPos, suit: cards::Suit, target: bid::Target) {
+        println!("Player {:?} bid {} on {}",
+                 pos,
+                 target.to_string(),
+                 suit.to_string());
+    }
+
+    fn ask_bid(&mut self) -> client::AuctionAction {
