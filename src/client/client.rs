ea5f44b66b5c4425321469c15bd049045db024c9
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2021-10-24 19:00:26 +0000

    fbcon: fix race condition between console lock and cursor timer (v1.1)

--- src/client/client.rs
+++ src/client/client.rs
@@ -3,3 +3,9 @@ use {PlayerEvent, EventType, ContractBody, CardBody};
 use super::{Backend, AuctionAction, Frontend, GameAction};
 
 pub struct Client<B: Backend> {
+    pub scores: [i32; 2],
+    backend: B,
+}
+
+enum GameError {
+  
