0245862735a410c32d752e096a37677b39a8ed18
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2021-10-24 11:51:49 +0000

    Updating version 0.0.4-SNAPSHOT with tag v0.0.4-SNAPSHOT

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,3 +1,5 @@
 use libcoinche::{cards, pos};
 use {PlayerEvent, EventType, ContractBody, CardBody};
-use super::{Backend, 
+use super::{Backend, AuctionAction, Frontend, GameAction};
+
+pub struct Client<B: Backend> {
