a1594aa8725bb714335a291e9ce49cd4b5d4ef10
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2022-01-24 14:31:19 +0000

    Qt3support--

--- src/client/http.rs
+++ src/client/http.rs
@@ -68,3 +68,81 @@ fn from_reader<R: Read, T: Decodable>(r: &mut R) -> Result<T, Error> {
 
     let has_error = match json.as_object() {
         Some(obj) => obj.contains_key("error"),
+        _ => false,
+    };
+
+    if has_error {
+        let mut decoder = json::Decoder::new(json);
+        let err = try!(Decodable::decode(&mut decoder));
+        Err(Error::Coinched(err))
+    } else {
+        let mut decoder = json::Decoder::new(json);
+        let result = try!(Decodable::decode(&mut decoder));
+        Ok(result)
+    }
+}
+
+/// Leave the party on drop.
+/// TODO: handle "soft" exit with reconnection?
+impl Drop for HttpBackend {
+    fn drop(&mut self) {
+        let leave_url = format!("http://{}/leave/{}", self.host, self.player_id);
+        hyper::Client::new().post(&leave_url).send().unwrap();
+    }
+}
+
+impl HttpBackend {
+    /// Creates a client to connect to the given server, once logged in.
+    fn new(host: &str, player_id: u32, pos: pos::PlayerPos) -> Self {
+
+        HttpBackend {
+            player_id: player_id,
+            pos: pos,
+            event_id: 0,
+            host: host.to_string(),
+        }
+    }
+
+    /// Attempt to join a game on the given host.
+    pub fn join(host: &str) -> Result<Self, Error> {
+        let client = hyper::Client::new();
+
+        let join_url = try!(format!("http://{}/join", host).into_url());
+        println!("Connecting to {}", host);
+        let mut response = try!(client.post(join_url).send());
+        let party: NewPartyInfo = try!(from_reader(&mut response));
+
+        Ok(HttpBackend::new(host, party.player_id, party.player_pos))
+    }
+
+    /// Parse and return an event from the given reader.
+    fn read_event<R: io::Read>(&mut self, r: &mut R) -> Result<EventType, Error> {
+        let event: Event = try!(from_reader(r));
+
+        self.event_id = event.id + 1;
+
+        Ok(event.event)
+    }
+}
+
+impl Backend for HttpBackend {
+    type Error = Error;
+
+    fn wait(&mut self) -> Result<EventType, Error> {
+        let wait_url = format!("http://{}/wait/{}/{}",
+                               &self.host,
+                               self.player_id,
+                               self.event_id);
+        let mut response = try!(hyper::Client::new().get(&wait_url).send());
+        self.read_event(&mut response)
+    }
+
+    fn bid(&mut self, contract: ContractBody) -> Result<EventType, Error> {
+        let bid_url = format!("http://{}/bid/{}", self.host, self.player_id);
+        let body = json::encode(&contract).unwrap();
+        let mut response = try!(hyper::Client::new()
+                                    .post(&bid_url)
+                                    .header(ContentType(Mime(TopLevel::Application,
+                                                             SubLevel::Json,
+                                                             vec![(Attr::Charset, Value::Utf8)])))
+                                    .body(&body)
