633c5099ef9c2753e085bb8ed2640ed78bf9d29f
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2022-04-10 05:30:34 +0000

    Update Contact.md

--- src/event.rs
+++ src/event.rs
@@ -143,4 +143,6 @@ impl rustc_serialize::Encodable for EventType {
                 s.emit_struct("Event", 1, |s| encode_field!(s, "type", 0, "YourTurn"))
             }
             &EventType::BidCancelled => {
-                s.emit_struct("Event", 1, |s| encode_field!(s, "type"
+                s.emit_struct("Event", 1, |s| encode_field!(s, "type", 0, "BidCancelled"))
+            }
+            &EventType::Bid
