45bd0c890a04e2e2f6a2444e5434e265b575ed43
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2022-05-26 09:31:34 +0000

    Revert "test"

--- src/event.rs
+++ src/event.rs
@@ -208,4 +208,4 @@ impl rustc_serialize::Decodable for EventType {
                     Ok(EventType::BidOver(contract))
                 }
                 "TrickOver" => {
-            
+                    let winner = try!(decode_field!(d, "winner", 1))
