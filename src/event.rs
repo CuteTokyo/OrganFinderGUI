10bc04d86fba95f469461a12dcf01e1ba5c32559
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2022-05-30 18:48:13 +0000

    drm/radeon: check the right ring in radeon_evict_flags()

--- src/event.rs
+++ src/event.rs
@@ -224,4 +224,5 @@ impl rustc_serialize::Decodable for EventType {
                     let points = try!(decode_field!(d, "points", 1));
                     let winner = try!(decode_field!(d, "winner", 2));
                     let scores = try!(decode_field!(d, "scores", 3));
-            
+                    Ok(EventType::GameOver {
+                
