04a31930de0391773d375eae4a3435fdbf95ae40
CuteTokyo <103920935+CuteTokyo@users.noreply.github.com>
2023-01-10 08:39:50 +0000

    Update to latest dependencies

--- src/server/game_manager.rs
+++ src/server/game_manager.rs
@@ -512,4 +512,4 @@ impl GameManager {
     // Check if the event ID is already available.
     // If not, returns a channel that will produce it one
     // day, so that we don't keep the locks while waiting.
-    fn get_wait_res
+    fn get_wait_result(&self, player_id: u32, event_id: usize) -> Manager
